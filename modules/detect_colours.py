f1ee2b784f7ea8740927400903e539a0c24db28f
thomasvilly <thomas.e.villeneuve@gmail.com>
2025-10-30 14:20:19 -0400

    colors picked

--- modules/detect_colours.py
+++ modules/detect_colours.py
@@ -8,11 +8,6 @@ from pathlib import Path
 import cv2
 import numpy as np
 
-# Bootcampers remove the following lines:
-# Allow linters and formatters to pass for bootcamp maintainers
-# pylint: disable=unused-argument,unused-variable,used-before-assignment
-
-
 class DetectBlue:
     """
     Detects blue objects from an image.
@@ -49,17 +44,17 @@ class DetectBlue:
         # ============
 
         # Convert the image's colour to HSV
-        hsv = ...
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         # Set upper and lower bounds for colour detection, this is in HSV
-        lower_blue = ...
-        upper_blue = ...
+        lower_blue = np.array([100,50,50])
+        upper_blue = np.array([150,250,250])
 
         # Apply the threshold for the colour detection
-        mask = ...
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
         # Shows the detected colour from the mask
-        res = ...
+        res = cv2.bitwise_and(hsv, hsv, mask=mask)
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -115,21 +110,21 @@ class DetectRed:
         # ============
 
         # Convert the image's colour to HSV
-        hsv = ...
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         # Set upper and lower bounds for colour detection, this is in HSV
-        lower_red = ...
-        upper_red = ...
+        lower_red = np.array([0,50,50])
+        upper_red = np.array([30,250,250])
 
         # Apply the threshold for the colour detection
-        mask = ...
+        mask = cv2.inRange(hsv, lower_red, upper_red)
 
         # Shows the detected colour from the mask
-        res = ...
+        res = cv2.bitwise_and(hsv, hsv, mask=mask)
 
         # Annotate the colour detections
         # replace the '_' parameter with the appropiate variable
-        contours, _ = cv2.findContours(_, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
         # ============
@@ -148,6 +143,7 @@ class DetectRed:
 
         # Include the "return_mask" parameter if statement here, similar to how it is implemented in DetectBlue
         # Tests will not pass if this isn't included!
+        return mask if return_mask else None
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
--- modules/quick_trial.py
+++ modules/quick_trial.py
@@ -4,6 +4,15 @@ Runs colour detection on sample image.
 
 import pathlib
 import time
+import os
+
+# debug modules.detect_colours import ... not being found
+# import sys
+
+# print(f"Current Working Directory: {os.getcwd()}")
+# print("\nPython Path (where Python looks for modules):")
+# for path in sys.path:
+#     print(f"- {path}")
 
 from modules.detect_colours import DetectBlue, DetectRed
 
@@ -13,9 +22,14 @@ OUTPUT_PATH = pathlib.Path("Output")
 OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
 IMAGE = "map_test.jpg"
 
+# fix relative path error
+script_dir = os.path.dirname(os.path.abspath(__file__))
+IMAGE = os.path.join(script_dir, IMAGE)
+
+
 # labels for red and blue colour detections
-BLUE_DETECTION = OUTPUT_PATH / f"blue_colour_detection_{time.time_ns()}.jpg"
-RED_DETECTION = OUTPUT_PATH / f"red_colour_detection_{time.time_ns()}.jpg"
+BLUE_DETECTION = OUTPUT_PATH / f"blue_colour_detection.jpg"
+RED_DETECTION = OUTPUT_PATH / f"red_colour_detection.jpg"
 
 blue_detector = DetectBlue.create()
 red_detector = DetectRed.create()
